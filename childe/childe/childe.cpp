f5037fbda1e6dedad9dd25e2c70cdcccdaf8854b
mehrad <y.nakhli1383@gmail.com>
2025-12-10 02:59:22 +0330

    final version

--- childe/childe/childe.cpp
+++ childe/childe/childe.cpp
@@ -1,94 +1,207 @@
-#include<iostream>
+﻿#include<iostream>
 #include<fstream>
 #include<vector>
 #include<random>
 #include<Windows.h>
+#include<chrono>
+#include<string>
 
 using namespace std;
 
 vector<long long int> readData();
+vector<int> INDEX;
+
+random_device ran;
+mt19937 gen(ran());
+
 int randMaker(int);
+bool timer(int);
+bool checkUsedIndex(int);
+bool findInVector(int);
 
-int main() {
-
-	TCHAR Name[] = TEXT("OSp01");
-	//HANDLE memory = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 100 * sizeof(long long int), Name);
-
-	HANDLE memory = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, Name);
+int main(int argc, char* argv[]) {
 
 
-	long long int *Buf = (long long int*)MapViewOfFile(memory, FILE_MAP_ALL_ACCESS, 0, 0, 100 * sizeof(long long int));
-	if (!Buf) {
-		CloseHandle(memory);
-		return 0;
+	if (argc < 2) {
+		cout << "Child received no index!" << endl;
+		return -1;
 	}
 
+	int resInedex= atoi(argv[1]); 
 
+
+	TCHAR Name3[] = TEXT("OSpIndex");
+	HANDLE memoryIndex = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, Name3);
+
+	int* BufIndex = (int*)MapViewOfFile(memoryIndex, FILE_MAP_ALL_ACCESS, 0, 0, 2 * sizeof(int));
+	if (!BufIndex) {
+		CloseHandle(memoryIndex);
+		exit(-1);
+
+	}
+
+	int logicalProcess = BufIndex[1];
 	vector <long long int> loadedData = readData();
-	vector <long long int> resualt = {0};
-
 	long long int mainNum = loadedData[0];
+	vector <long long int> resualt ;
 	long long int dataSum = 0;
-	int max = 100;
-	
-	do {
-		int index = randMaker(max);
-		long long int randVal = loadedData[index];
-		dataSum = dataSum + randVal;
-		resualt.push_back(randVal);
-		loadedData.erase(loadedData.begin() + (index));
-		max--;
-
-	} while (dataSum <= mainNum);
-
+	long long int prevDist = mainNum;
 	long long int dist = 0;
-	dist = dataSum - mainNum;
-	resualt[0] = dist;
+	string sResualt = "";
 
+	TCHAR Name2[] = TEXT("OSpRes");
+	HANDLE memoryRes = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, Name2);
+
+	long long int* BufRes = (long long int*)MapViewOfFile(memoryRes, FILE_MAP_ALL_ACCESS, 0, 0, loadedData.size() * logicalProcess * sizeof(long long int));
+	if (!BufRes) {
+		CloseHandle(memoryRes);
+		exit(-1);
+
+	}
+
+	while (timer(BufIndex[0]))
+	{
+		dataSum = 0;
+		vector<long long int> tempResult = { prevDist };
+
+		int index;
+		do {
+			do {
+			 index = randMaker(loadedData.size());
+
+			} while (checkUsedIndex(index));
+
+			long long int randVal = loadedData[index];
+			dataSum = dataSum + randVal;
+			tempResult.push_back(randVal);
+
+		} while (dataSum <= mainNum);
+		//check same index[()]
+
+		dist = dataSum - mainNum;
+
+
+		if (dist < prevDist)
+		{
+			prevDist = dist;
+			tempResult[0] = prevDist;
+			resualt = tempResult;
+
+		}
+		//string sresualt;
+		//sResualt = to_string(prevDist) + ": ";
+
+		//for (int i = 1;i < resualt.size();i++)
+		//{
+		//	sResualt += to_string(resualt[i]);
+		//	if (i != resualt.size() - 1)
+		//		sResualt += " + ";
+		//}
+
+		tempResult.clear();
+		INDEX.clear();
+	}
+
+	//cout << "final>>>>>>>>" << sResualt << "\n\n\n";
+
+	//cout << "My index is: " << resInedex << endl;
+
+
+	if (!resualt.empty()) 
 	for (int i = 0;i < resualt.size();i++) {
-		Buf[i] = resualt[i];
+		BufRes[(resInedex * loadedData.size()) + i] = resualt[i];
 	}
 
+	UnmapViewOfFile(BufIndex);
+	CloseHandle(memoryIndex);
 
-
-
-	cout << "distance= " << dist<<'\n'<<"values: ";
-	for (int j = 1;j < resualt.size();j++) {
-		if (j==resualt.size()-1)
-		cout << resualt[j] <<'\n';
-		else
-			cout << resualt[j] << " + ";
-
-	}
-	
-	CloseHandle(memory);
-	UnmapViewOfFile(Buf);
-	
+	UnmapViewOfFile(BufRes);
+	CloseHandle(memoryRes);
 
 	return 0;
 }
 
 vector<long long int> readData()
 {
-	ifstream dataFile("C:\\Users\\SHARIF\\Desktop\\os\\osp01\\childe\\childe\\data.txt");
-	if (!dataFile) {
-		cout << "we have a problem when trying open file....";
+	TCHAR Name[] = TEXT("OSp01");
+	HANDLE memory = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, Name);
+
+	long long int* Buf = (long long int*)MapViewOfFile(memory, FILE_MAP_ALL_ACCESS, 0, 0, 100 * sizeof(long long int));
+	if (!Buf) {
+		CloseHandle(memory);
 		exit(-1);
 	}
 
 	vector<long long int> numbers;
-	long long int fileMember;
 
-	while (dataFile >> fileMember) {
-		numbers.push_back(fileMember);
+	for (int i = 0; Buf[i];i++) {
+		numbers.push_back(Buf[i]);
 	}
+
 	return numbers;
 }
 
+//vector<long long int> readData()
+//{
+//	ifstream dataFile("C:\\Users\\SHARIF\\Desktop\\os\\osp01\\childe\\childe\\data.txt");
+//	if (!dataFile) {
+//		cout << "we have a problem when trying open file....";
+//		exit(-1);
+//	}
+//
+//	vector<long long int> numbers;
+//	long long int fileMember;
+//
+//	while (dataFile >> fileMember) {
+//		numbers.push_back(fileMember);
+//	}
+//	return numbers;
+//}
+
 int randMaker(int max) {
-	random_device ran;
-	mt19937 gen(ran());
-	uniform_int_distribution<int> dist(1, max);
+	
+	uniform_int_distribution<int> dist(1, max-1);
 	int randNum = dist(gen);
 	return randNum;
+}
+
+bool timer(int seconds)
+{
+	static bool started = false;
+	static auto start = std::chrono::steady_clock::now();
+
+	if (!started) {
+		start = std::chrono::steady_clock::now();
+		started = true;
+	}
+
+	auto elapsed = std::chrono::steady_clock::now() - start;
+
+	if (elapsed < std::chrono::seconds(seconds))
+		return true;
+
+	started = false;
+	return false;
+}
+
+bool checkUsedIndex(int index)
+{
+	if (findInVector(index)) {
+		return true;
+	}
+	else {
+		INDEX.push_back(index);
+		return false;
+	}
+	
+}
+
+bool findInVector(int index)
+{
+	for (int i = 0;i < INDEX.size();i++) {
+		if (INDEX[i] == index) {
+			return true;
+		}
+	}
+	return false;
 }
--- parent/parent/parent.cpp
+++ parent/parent/parent.cpp
@@ -1,10 +1,26 @@
-#include<iostream>
+﻿#include<iostream>
 #include<Windows.h>
 #include<vector>
+#include<fstream>
+#include<chrono>
+#include<string>
+
 using namespace std;
 
+vector<long long int> readData();
+vector <long long int> loadedData = readData();
+vector<long long int> port;
+string values(int);
+bool timer(int);
+
+struct bestResult {
+	int chlideNum;
+	long long int distance;
+	string fullResult;
+};
 
 int main() {
+
 	SYSTEM_INFO core;
 	GetSystemInfo(&core);
 	int logicalProcess = core.dwNumberOfProcessors;
@@ -15,56 +31,178 @@ int main() {
 	ZeroMemory(&s, sizeof(s));
 	s.cb = sizeof(s);
 	ZeroMemory(&p, sizeof(p));
-
+	///////////////////////////////////////////////////////////////memory
 	TCHAR Name[] = TEXT("OSp01");
-	HANDLE memory= CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 100 * sizeof(long long int), Name);
+	HANDLE memory = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, loadedData.size() * sizeof(long long int), Name);
+
+	TCHAR Name2[] = TEXT("OSpRes");
+	HANDLE memoryRes = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, loadedData.size() * logicalProcess * sizeof(long long int), Name2);
+
+	TCHAR Name3[] = TEXT("OSpIndex");
+	HANDLE memoryIndex = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 1 * sizeof(int), Name3);
 
 
-	long long int* Buf = (long long int*)MapViewOfFile(memory, FILE_MAP_ALL_ACCESS, 0, 0, 100 * sizeof(long long int));
+	long long int* Buf = (long long int*)MapViewOfFile(memory, FILE_MAP_ALL_ACCESS, 0, 0, loadedData.size() * sizeof(long long int));
 	if (!Buf) {
 		CloseHandle(memory);
-		return 1;
+		exit(-1);
+
 	}
 
-	long long int finalResualt[100] = { 0 };
+	long long int* BufRes = (long long int*)MapViewOfFile(memoryRes, FILE_MAP_ALL_ACCESS, 0, 0, loadedData.size() * logicalProcess * sizeof(long long int));
+	if (!BufRes) {
+		CloseHandle(memoryRes);
+		exit(-1);
+
+	}
+
+	int* BufIndex = (int*)MapViewOfFile(memoryIndex, FILE_MAP_ALL_ACCESS, 0, 0, 2 * sizeof(int));
+	if (!BufIndex) {
+		CloseHandle(memoryIndex);
+		exit(-1);
+
+	}
+	///////////////////////////////////////////////////
+
+	for (int i = 0;i < loadedData.size();i++) {
+		Buf[i] = loadedData[i];
+	}
+
 	long long int minDist = 8e17;
-	
+	int timeAsSecond;
+	cout << "inpt a time for run (seconds):\n";
+	cin >> timeAsSecond;
+	system("cls");
+
+	BufIndex[0] = timeAsSecond;
+	BufIndex[1] = logicalProcess;
+
 	for (int i = 0;i < logicalProcess;i++) {
-		ZeroMemory(Buf, 100 * sizeof(long long int));
+
+		TCHAR cmd[128];
+		wsprintf(cmd, TEXT("C:\\Users\\SHARIF\\Desktop\\os\\osp01\\childe\\x64\\Release\\childe.exe %d"), i);
 
 		bool b = CreateProcess(TEXT("C:\\Users\\SHARIF\\Desktop\\os\\osp01\\childe\\x64\\Release\\childe.exe"),
-			NULL, NULL, NULL, FALSE, 0, NULL, NULL, &s, &p);
+			cmd, NULL, NULL, FALSE, 0, NULL, NULL, &s, &p);
 
 		if (!b)
 		{
 			cout << "Create Process failed" << endl;
 			return 1;
 		}
-		
-		
-		// Wait until child process exits.
-		
-		WaitForSingleObject(p.hProcess, INFINITE);
-		cout << "-------------------------------------------------\n";
+		CloseHandle(p.hProcess);
+	}
 
-		if (Buf[0] < minDist) {
-			minDist = Buf[0];
-			
-			for (int i = 0;i<100;i++) {
-				finalResualt[i] = 0;
-			}
-			for (int j = 1;Buf[j] != 0;j++) {
-				finalResualt[j-1]=Buf[j];
-			}
+	while (timer(timeAsSecond + 2)) {
+		cout << "loading\r";
+		Sleep(500);
+		cout << "loading.\r";
+		Sleep(500);
+		cout << "loading..\r";
+		Sleep(500);
+		cout << "loading...\r";
+		Sleep(500);
+		system("cls");
+
+	}
+	system("cls");
+
+	bestResult bs;
+	bestResult tempp;
+	bs.distance = minDist;
+
+	for (int i = 0;i < logicalProcess*loadedData.size();i++) {
+		port.push_back(BufRes[i]);
+	}
+	//////////////////////////////print childe
+
+	//for (int i = 0;i < 2020;i++) {
+	//	if (i % 101 == 0) {
+	//		cout << "--------------------------------------------------------\n";
+	//	}
+	//		cout << port[i]<<"\n";
+	//}
+
+	for (int i = 0;i < logicalProcess;i++) {
+
+		/*int waitCounter = 0;
+		while (BufRes[i * loadedData.size()] == 0 && waitCounter < 5000) {
+			Sleep(1);
+			waitCounter++;
+		}*/
+
+		tempp.distance = port[i * loadedData.size()];
+		tempp.chlideNum = i + 1;
+		tempp.fullResult = values(tempp.chlideNum);
+
+
+		//cout << "child <" << i + 1 << ">-------------------------------------\n\n"+ values(i+1);
+
+		if (bs.distance > tempp.distance) {
+			bs = tempp;
 		}
 	}
-	cout << "-------------------------------------------------\n";
-	cout << ">>min<< ( " << minDist << " )\n";
-	for (int j = 0;finalResualt[j]!=0;j++) {
-		if ( finalResualt[j+1]==0)
-			cout << finalResualt[j] << '\n';
+
+	cout << "--------------------------------------------------------\n";
+	cout << ">>min<< ( " << bs.distance << " )\n\n>>from childe<< ((" << bs.chlideNum << "))\n\n>>with this values<< \n\n" << bs.fullResult<<"\n\n";
+
+	UnmapViewOfFile(Buf);
+	CloseHandle(memory);
+
+	UnmapViewOfFile(BufRes);
+	CloseHandle(memoryRes);
+
+	UnmapViewOfFile(BufIndex);
+	CloseHandle(memoryIndex);
+	return 0;
+}
+
+vector<long long int> readData()
+{
+	ifstream dataFile("C:\\Users\\SHARIF\\Desktop\\os\\osp01\\childe\\childe\\data.txt");
+	if (!dataFile) {
+		cout << "we have a problem when trying open file....";
+		exit(-1);
+	}
+
+	vector<long long int> numbers;
+	long long int fileMember;
+
+	while (dataFile >> fileMember) {
+		numbers.push_back(fileMember);
+	}
+	return numbers;
+}
+
+string values(int childeNum)
+{
+	string path = "";
+	for (int i = ((childeNum - 1) * loadedData.size()+1);port[i] != 0;i++) {
+		if (port[i+1]!=0)
+		path += to_string(port[i])+" + ";
 		else
-			cout << finalResualt[j] << " + ";
+		path += to_string(port[i])+"\n\n";
 
 	}
+	return path;
 }
+
+bool timer(int seconds)
+{
+	static bool started = false;
+	static auto start = std::chrono::steady_clock::now();
+
+	if (!started) {
+		start = std::chrono::steady_clock::now();
+		started = true;
+	}
+
+	auto elapsed = std::chrono::steady_clock::now() - start;
+
+	if (elapsed < std::chrono::seconds(seconds))
+		return true;
+
+	started = false;
+	return false;
+}
+
